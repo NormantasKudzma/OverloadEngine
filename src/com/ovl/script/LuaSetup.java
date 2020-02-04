f3db1b425ebc6921361f4e92233c57f5325ef756
Normantas <kudzma.normantas@gmail.com>
2020-02-04 18:10:56 +0200

    Expose game engine to lua

--- src/com/ovl/script/LuaSetup.java
+++ src/com/ovl/script/LuaSetup.java
@@ -3,6 +3,7 @@ package com.ovl.script;
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 
+import com.ovl.engine.OverloadEngine;
 import com.ovl.game.BaseGame;
 import com.ovl.game.GameObject;
 import com.ovl.graphics.Sprite;
@@ -14,6 +15,7 @@ public class LuaSetup {
 	public static void setup() {
 		Globals globals = Lua.globals();
 		
+		globals.set("Engine", CoerceJavaToLua.coerce(OverloadEngine.getInstance()));
 		globals.set("Paths", CoerceJavaToLua.coerce(Paths.class));
 		
 		globals.set("GameObject", CoerceJavaToLua.coerce(GameObject.class));
